77a03ac3dd947355d28441228a8509754830db14
Ismo Puustinen <ismo.h.puustinen@nokia.com>
2008-07-11 14:59:39 +0300

    HAL changes towards capability-based filtering

--- plugins/hal/hal.c
+++ plugins/hal/hal.c
@@ -16,6 +16,64 @@ OHM_DEBUG_PLUGIN(hal,
 
 hal_plugin *hal_plugin_p = NULL;
 
+
+
+gboolean headset_cb (OhmFact *hal_fact, gchar *capability, gboolean added, gboolean removed, void *user_data) {
+
+    gchar *fact_name = "com.nokia.policy.accessories";
+    OhmFact *fact = NULL;
+    OhmFactStore *fs = ohm_fact_store_get_fact_store();
+    GSList *list = NULL;
+    GValue *udi_val_hal, *udi_val_fs;
+    GValue *val_s = NULL, *val_i = NULL;
+    /* gchar *udi = NULL; */
+
+    printf("Hal headset event received!\n");
+
+    list = ohm_fact_store_get_facts_by_name(fs, fact_name);
+
+    udi_val_hal = ohm_fact_get(hal_fact, "udi");
+
+    if (g_slist_length(list) == 0) {
+        fact = ohm_fact_new("com.nokia.policy.accessories");
+        udi_val_fs = ohm_copy_value(udi_val_hal);
+        ohm_fact_set(fact, "udi", udi_val_fs);
+    }
+    else if (g_slist_length(list) == 1) {
+        fact = list->data;
+    }
+    else {
+        printf("More than one facts (%i) found!\n", g_slist_length(list));
+        OHM_DEBUG(DBG_FACTS, "The requested fact was not found");
+        /* FIXME: use the first fact */
+        fact = list->data;
+
+        /* go through the facts and find the one with correct udi,
+         * meaning we are changing only one value */
+    }
+
+    if (!(added || removed) || added) {
+        /* during decoration */
+        printf("\nHeadset inserted!\n");
+        val_i = ohm_value_from_int(1);
+        val_s = ohm_value_from_string("headset");
+        ohm_fact_set(fact, "device", val_s);
+        ohm_fact_set(fact, "state", val_i);
+    }
+    else {
+        /* removed */
+        printf("\nHeadset removed!\n");
+        val_i = ohm_value_from_int(0);
+        val_s = ohm_value_from_string("headset");
+        ohm_fact_set(fact, "device", val_s);
+        ohm_fact_set(fact, "state", val_i);
+    }
+
+    ohm_fact_store_insert(fs, fact);
+
+    return TRUE;
+}
+
 static void
 plugin_init(OhmPlugin * plugin)
 {
@@ -28,24 +86,24 @@ plugin_init(OhmPlugin * plugin)
     hal_plugin_p = init_hal(c, DBG_HAL, DBG_FACTS);
     OHM_DEBUG(DBG_HAL, "< HAL plugin init");
 
+    /* FIXME HACK */
+
+    /* Let's make the headset hardcoded for testing purposes :-P */
+
+    decorate(hal_plugin_p, "volume.disc", headset_cb, NULL);
+
     return;
 }
 
-/**
- * Marks the udi as interesting. Interesting HAL devices are mapped to
- * factstore.
- */
-OHM_EXPORTABLE(gboolean, interested, (gchar *udi))
+
+OHM_EXPORTABLE(gboolean, set_decorator, (gchar *regexp, hal_cb cb, void *user_data))
 {
-    return mark_interesting(hal_plugin_p, udi);
+    return decorate(hal_plugin_p, regexp, cb, user_data);
 }
 
-/**
- * Marks the udi as uninteresting.
- */
-OHM_EXPORTABLE(gboolean, uninterested, (gchar *udi))
+OHM_EXPORTABLE(gboolean, unset_decorator, (void *user_data))
 {
-    return mark_uninteresting(hal_plugin_p, udi);
+    return undecorate(hal_plugin_p, user_data);
 }
 
 static void
@@ -65,8 +123,9 @@ OHM_PLUGIN_DESCRIPTION("hal",
         NULL);
 
 OHM_PLUGIN_PROVIDES_METHODS(hal, 2,
-        OHM_EXPORT(interested, "interested"),
-        OHM_EXPORT(uninterested, "uninterested"));
+        OHM_EXPORT(unset_decorator, "unset_decorator"),
+        OHM_EXPORT(set_decorator, "set_decorator"));
+
 /*
  * Local Variables:
  * c-basic-offset: 4
