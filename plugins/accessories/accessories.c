defc1cd03247b169bb502c098a6d80653be13777
Janos Kovacs <janos.f.kovacs@nokia.com>
2008-11-24 00:44:43 +0200

    - support for the resolver based interface to administer connected/disconnected   accessories - the B-Bus info signal now can have an optional 'driver' or 'connected' value   in front of the state (e.g the '1' or '0'). If neither 'driver' nor   'connected' is present the 'driver' field will be updated, i.e. the old   message format will update the 'driver' field.

--- plugins/accessories/accessories.c
+++ plugins/accessories/accessories.c
@@ -15,18 +15,50 @@ static OhmFactStore  *fs;
 static gboolean headset_init(OhmPlugin *plugin);
 static gboolean headset_deinit(OhmPlugin *plugin);
 
-static void update_factstore_entry(char *, char *, char *);
-static const char *get_field(OhmFact *, char *);
+#if 0
+static void update_factstore_entry(char *, char *, int);
+static const char *get_string_field(OhmFact *, char *);
+static int get_integer_field(OhmFact *, char *);
+#endif
+
+static int dres_accessory_request(char *, int, int);
 
 typedef gboolean (*hal_cb) (OhmFact *hal_fact, gchar *capability, gboolean added, gboolean removed, void *user_data);
 
+#ifndef __NO_HAL__
+OHM_IMPORTABLE(int, resolve, (char *goal, char **locals));
 OHM_IMPORTABLE(gboolean, set_observer, (gchar *capability, hal_cb cb, void *user_data));
 OHM_IMPORTABLE(gboolean, unset_observer, (void *user_data));
 
-OHM_PLUGIN_REQUIRES_METHODS(accessories, 2, 
+
+OHM_PLUGIN_REQUIRES_METHODS(accessories, 3, 
+   OHM_IMPORT("dres.resolve", resolve),
    OHM_IMPORT("hal.set_observer", set_observer),
    OHM_IMPORT("hal.unset_observer", unset_observer)
 );
+#else
+OHM_IMPORTABLE(int, resolve, (char *goal, char **locals));
+
+static gboolean set_observer(gchar *capability, hal_cb cb, void *userdata)
+{
+    (void)capability;
+    (void)cb;
+    (void)userdata;
+
+    return 1;
+}
+
+static gboolean unset_observer(void *userdata)
+{
+    (void)userdata;
+
+    return 1;
+}
+
+OHM_PLUGIN_REQUIRES_METHODS(accessories, 1, 
+   OHM_IMPORT("dres.resolve", resolve)
+);
+#endif
 
 static void plugin_init(OhmPlugin *plugin)
 {
@@ -55,14 +87,20 @@ static void plugin_exit(OhmPlugin *plugin)
  *    if neccessary
  **/
 
+#if 0                           /* old version */
 gboolean headset_cb (OhmFact *hal_fact, gchar *capability, gboolean added, gboolean removed, void *user_data)
 {
     GValue *val_i = NULL, *capabilities = NULL;
     OhmFact *fact = NULL;
-    gchar *fact_name = "com.nokia.policy.accessories";
+    gchar *fact_name = "com.nokia.policy.audio_device_connected";
     GSList *i = NULL, *list = NULL;
     int state = 0;
     gboolean found = FALSE;
+
+    (void)capability;
+    (void)added;
+    (void)removed;
+    (void)user_data;
 
     /* printf("Possible hal headset event received!\n"); */
 
@@ -73,7 +111,7 @@ gboolean headset_cb (OhmFact *hal_fact, gchar *capability, gboolean added, gbool
     for (i = list; i != NULL; i = g_slist_next(i)) {
         OhmFact *of = i->data;
 
-        GValue *gval = ohm_fact_get(of, "device");
+        GValue *gval = ohm_fact_get(of, "name");
 
         if (G_VALUE_TYPE(gval) == G_TYPE_STRING) {
             const gchar *value = g_value_get_string(gval);
@@ -130,7 +168,7 @@ gboolean headset_cb (OhmFact *hal_fact, gchar *capability, gboolean added, gbool
 
                                 /* change the virtual fact */
                                 val_i = ohm_value_from_int(1);
-                                ohm_fact_set(fact, "state", val_i);
+                                ohm_fact_set(fact, "connected", val_i);
 
                                 found = TRUE;
                                 break;
@@ -140,7 +178,78 @@ gboolean headset_cb (OhmFact *hal_fact, gchar *capability, gboolean added, gbool
 
                             /* change the virtual fact */
                             val_i = ohm_value_from_int(0);
-                            ohm_fact_set(fact, "state", val_i);
+                            ohm_fact_set(fact, "connected", val_i);
+
+                            found = TRUE;
+                            break;
+                        }
+                        /* else redundant event */
+                    }
+                }
+            }
+        }
+        g_strfreev(caps);
+    }
+
+    return found;
+}
+#endif
+
+gboolean headset_cb (OhmFact *hal_fact, gchar *capability, gboolean added, gboolean removed, void *user_data)
+{
+    GValue   *capabilities = NULL;
+    int       state = 0;
+    gboolean  found = FALSE;
+
+    (void)capability;
+    (void)added;
+    (void)removed;
+    (void)user_data;
+
+    /* printf("Possible hal headset event received!\n"); */
+
+    capabilities = ohm_fact_get(hal_fact, "info.capabilities");
+
+    if (capabilities == NULL) {
+        /* printf("Headset removed or something\n"); */
+    }
+    else if (G_VALUE_TYPE(capabilities) == G_TYPE_STRING) {
+        const gchar *escaped_caps = g_value_get_string(capabilities);
+#define STRING_DELIMITER "\\"
+        gchar **caps = g_strsplit(escaped_caps, STRING_DELIMITER, 0);
+#undef STRING_DELIMITER
+        gchar **caps_iter = caps;
+        
+        for (; *caps_iter != NULL; caps_iter++) {
+            gchar *cap = *caps_iter;
+
+            if (cap && strcmp(cap, "button") == 0) {
+                GValue *gval_b = ohm_fact_get(hal_fact, "button.state.value");
+                GValue *gval_id = ohm_fact_get(hal_fact, "platform.id");
+
+                if (gval_b &&
+                    G_VALUE_TYPE(gval_b) == G_TYPE_INT &&
+                    gval_id &&
+                    G_VALUE_TYPE(gval_id) == G_TYPE_STRING)
+                {
+                    gboolean value_b = g_value_get_int(gval_b);
+                    const gchar *value_id = g_value_get_string(gval_id);
+                
+                    if (!strcmp(value_id, "headphone")) {
+                        /* printf("Fact has the headset capability\n");
+                         */
+                        if (value_b && !state) {
+                                printf("Headset inserted!\n");
+
+                                dres_accessory_request("headset", -1, 1);
+
+                                found = TRUE;
+                                break;
+                            }
+                        else if (!value_b && state) {
+                            printf("Headset removed!\n");
+
+                            dres_accessory_request("headset", -1, 0);
 
                             found = TRUE;
                             break;
@@ -158,11 +267,15 @@ gboolean headset_cb (OhmFact *hal_fact, gchar *capability, gboolean added, gbool
 
 static gboolean headset_deinit(OhmPlugin *plugin)
 {
+    (void)plugin;
+
     return unset_observer(token);
 }
 
 static gboolean headset_init(OhmPlugin *plugin)
 {
+    (void)plugin;
+
     return set_observer("button", headset_cb, token);
 }
 
@@ -170,21 +283,54 @@ static gboolean headset_init(OhmPlugin *plugin)
 
 static DBusHandlerResult info(DBusConnection *c, DBusMessage * msg, void *data)
 {
-    static char     *factname = "com.nokia.policy.accessories";
-
+    int              driver    = -1;
+    int              connected = -1;
+    int             *valueptr  = &driver;
+    int              value     = -1;
     DBusMessageIter  msgit;
     DBusMessageIter  devit;
-    char            *state;
+    char            *string;
+    char            *end;
     char            *device;
+
+    (void)c;
+    (void)data;
 
     if (dbus_message_is_signal(msg, "com.nokia.policy", "info")) {
 
         dbus_message_iter_init(msg, &msgit);
 
-        if (dbus_message_iter_get_arg_type(&msgit) != DBUS_TYPE_STRING)
-            goto done;
+        for (;;) {
+            if (dbus_message_iter_get_arg_type(&msgit) != DBUS_TYPE_STRING)
+                goto done;
 
-        dbus_message_iter_get_basic(&msgit, (void *)&state);
+            dbus_message_iter_get_basic(&msgit, (void *)&string);
+
+            if (!strcmp(string, "driver")) {
+                valueptr = &driver;
+
+                if (!dbus_message_iter_next(&msgit))
+                    goto done;
+            } else
+
+            if (!strcmp(string, "connected")) {
+                valueptr = &connected;
+
+                if (!dbus_message_iter_next(&msgit))
+                    goto done;
+            }
+            
+            else {
+                value = strtol(string, &end, 10);
+
+                if (*end == '\0' && (value == 0 || value == 1)) {
+                    *valueptr = value;
+                    break;
+                }
+
+                goto done;
+            }
+        }
         
         if (!dbus_message_iter_next(&msgit) ||
             dbus_message_iter_get_arg_type(&msgit) != DBUS_TYPE_ARRAY)
@@ -198,7 +344,7 @@ static DBusHandlerResult info(DBusConnection *c, DBusMessage * msg, void *data)
 
             dbus_message_iter_get_basic(&devit, (void *)&device);
 
-            update_factstore_entry(factname, device, state);
+            dres_accessory_request(device, driver, connected);
       
         } while (dbus_message_iter_next(&devit));
 
@@ -209,15 +355,16 @@ static DBusHandlerResult info(DBusConnection *c, DBusMessage * msg, void *data)
     return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
 }
 
-static void update_factstore_entry(char *factname, char *device, char *state)
+#if 0
+static void update_factstore_entry(char *factname, char *device, int driver)
 {
     OhmFact     *fact;
     GSList      *list;
     GValue      *gv;
     const char  *fdev;
-    const char  *fstat;
+    int          fdrv;
 
-    printf("%s: %s(%s, %s)\n", __FILE__, __FUNCTION__, device, state);
+    printf("%s: %s(%s, %d)\n", __FILE__, __FUNCTION__, device, driver);
 
     for (list  = ohm_fact_store_get_facts_by_name(fs, factname);
          list != NULL;
@@ -225,27 +372,28 @@ static void update_factstore_entry(char *factname, char *device, char *state)
     {
         fact = (OhmFact *)list->data;
 
-        if ((fdev  = get_field(fact, "device")) != NULL &&
-            (fstat = get_field(fact, "state"))  != NULL    ) {
+        if ((fdev = get_string_field(fact, "name")) != NULL) {
 
             if (!strcmp(fdev, device)) {
-                if (!strcmp(fstat, state)) {
-                    printf("%s: '%s' device state is already '%s'",
-                           __FILE__, device, state);
+                if ((fdrv = get_integer_field(fact, "driver")) == driver) {
+                    printf("%s: '%s' device driver state is already %d",
+                           __FILE__, device, driver);
                 }
                 else {
-                    gv = ohm_value_from_string(state);
-                    ohm_fact_set(fact, "state", gv);
+                    gv = ohm_value_from_int(driver);
+                    ohm_fact_set(fact, "driver", gv);
                 }
 
                 return;         /* supposed to have just one */
             }
         }
-        
     }
+
+    printf("%s: %s(), there is no fact with name='%s'\n",
+           __FILE__, __FUNCTION__, device);
 }
 
-static const char *get_field(OhmFact *fact, char *name)
+static const char *get_string_field(OhmFact *fact, char *name)
 {
     GValue  *gv;
 
@@ -257,6 +405,56 @@ static const char *get_field(OhmFact *fact, char *name)
 
     return NULL;
 }
+
+static int get_integer_field(OhmFact *fact, char *name)
+{
+    GValue  *gv;
+
+    if ((gv = ohm_fact_get(fact, name)) != NULL) {
+        if (G_VALUE_TYPE(gv) == G_TYPE_INT) {
+            return g_value_get_int(gv);
+        }
+    }
+
+    return 0;
+}
+#endif
+
+static int dres_accessory_request(char *name, int driver, int connected)
+{
+#define DRES_VARTYPE(t)  (char *)(t)
+#define DRES_VARVALUE(s) (char *)(s)
+
+    static char *goal = "accessory_request";
+
+    char *vars[48];
+    int   i;
+    int   err;
+
+    vars[i=0] = "accessory_name";
+    vars[++i] = DRES_VARTYPE('s');
+    vars[++i] = DRES_VARVALUE(name);
+
+    vars[++i] = "accessory_driver";
+    vars[++i] = DRES_VARTYPE('i');
+    vars[++i] = DRES_VARVALUE(driver);
+
+    vars[++i] = "accessory_connected";
+    vars[++i] = DRES_VARTYPE('i');
+    vars[++i] = DRES_VARVALUE(connected);
+
+    vars[++i] = NULL;
+
+    if ((err = resolve(goal, vars)) != 0)
+        printf("%s: %s() resolving '%s' failed: (%d) %s",
+               __FILE__, __FUNCTION__, goal, err, strerror(err));
+
+    return err ? FALSE : TRUE;
+
+#undef DRES_VARVALUE
+#undef DRES_VARTYPE
+}
+
 
 OHM_PLUGIN_DESCRIPTION("accessories",
                        "0.0.1",
