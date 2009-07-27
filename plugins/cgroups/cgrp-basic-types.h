f56310490598f39d5322fe725e982671248ce7b9
Krisztian Litkey <krisztian.litkey@nokia.com>
2009-07-27 22:33:10 +0300

    Added missing headers.

---
+++ plugins/cgroups/cgrp-basic-types.h
@@ -0,0 +1,24 @@
+#ifndef __CGRP_BASIC_TYPES_H__
+#define __CGRP_BASIC_TYPES_H__
+
+#include <sys/types.h>
+
+typedef unsigned char  u8_t;
+typedef   signed char  s8_t;
+typedef unsigned short u16_t;
+typedef   signed short s16_t;
+typedef u_int32_t      u32_t;
+typedef   int32_t      s32_t;
+typedef u_int64_t      u64_t;
+typedef   int64_t      s64_t;
+
+#endif /* __CGRP_BASIC_TYPES_H__ */
+
+
+/* 
+ * Local Variables:
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ * vim:set expandtab shiftwidth=4:
+ */
---
+++ plugins/cgroups/cgrp-parser-types.h
@@ -0,0 +1,34 @@
+#ifndef __PARSER_TYPES_H__
+#define __PARSER_TYPES_H__
+
+#include "cgrp-basic-types.h"
+
+#define COMMON_TOKEN_FIELDS                     \
+    const char *token;                          \
+    int         lineno
+
+typedef struct {
+    COMMON_TOKEN_FIELDS;
+} token_any_t;
+
+typedef struct {
+    COMMON_TOKEN_FIELDS;
+    char *value;
+} token_string_t;
+
+typedef struct {
+    COMMON_TOKEN_FIELDS;
+    u32_t value;
+} token_uint32_t;
+
+#endif /* __PARSER_TYPES_H__ */
+
+
+
+/* 
+ * Local Variables:
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ * vim:set expandtab shiftwidth=4:
+ */
