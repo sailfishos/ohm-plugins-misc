57451a026c2103c92aa4b26bc3ce6ccede5c4836
Krisztian Litkey <krisztian.litkey@nokia.com>
2009-07-29 03:37:58 +0300

    Added not quite functional cgrp-facts.c.

---
+++ plugins/cgroups/cgrp-facts.c
@@ -0,0 +1,8 @@
+/*
+ * Brains: No Pinky, tonight we go to sleep. But tomorrow... what do we do
+ *         tomorrow ?
+ * Pinky:  Eat more cheese ?
+ * Brains: (slams Pinky on the head) No Pinky, the same thing we do every day...
+ * Pinky:  Aaah... Tomorrow we take over the world.
+ */
+
