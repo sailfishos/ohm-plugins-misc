79073ffe8a41aa3827ab3f91e9ad25a10e526f9d
Krisztian Litkey <krisztian.litkey@nokia.com>
2010-02-15 17:59:56 +0200

    Added all-or-nothing vibra enforcement to the immersion backend.

--- plugins/vibra/vibra-driver-immts.c
+++ plugins/vibra/vibra-driver-immts.c
@@ -13,16 +13,29 @@
 #define IMMTS_EVENT        (VIBE_MAX_OEM_DEVICE_PRIORITY    )
 #define IMMTS_ALL           0x7fffffff
 
+#define IMMTS_VOLUME_OFF    0
+#define IMMTS_VOLUME_MAX    10000
+
 #define MASK_RANGE(l, h) \
-    ((((((mask_t)-1) >> (l)) << (l)) << (31-(h))) >> (31-(h)))
+    ((((((uint32_t)-1) >> (l)) << (l)) << (31-(h))) >> (31-(h)))
 #define MASK_VALUE(val) (1 << (val))
 
-typedef uint32_t mask_t;
-
 typedef struct {
-    char   *name;                              /* group name */
-    mask_t  mask;                              /* TouchSense priority mask */
+    char     *name;                              /* group name */
+    uint32_t  mask;                              /* TouchSense priority mask */
 } immts_group_t;
+
+
+
+static int vibra_open(void);
+static void vibra_close(VibeInt32);
+static int vibra_auth(VibeInt32, const char *);
+static int vibra_set_priority(VibeInt32, int);
+#ifdef VIBE_DEVPROPTYPE_PRIORITY_MASK
+static int vibra_enable_priorities(VibeInt32, uint32_t);
+#endif
+static int vibra_mute(VibeInt32, int);
+
 
 
 #define VIBRA_GROUP(_type, _name, _mask) \
@@ -38,8 +51,10 @@ static immts_group_t immts_groups[] = {
 };
 
 
-static int       disabled = FALSE;
-static VibeInt32 dev      = VIBE_INVALID_DEVICE_HANDLE_VALUE;
+static VibeInt32 dev = VIBE_INVALID_DEVICE_HANDLE_VALUE;
+
+
+
 
 
 /********************
@@ -48,35 +63,27 @@ static VibeInt32 dev      = VIBE_INVALID_DEVICE_HANDLE_VALUE;
 void
 immts_init(vibra_context_t *ctx, OhmPlugin *plugin)
 {
-    VibeStatus  status;
-    const char  *key;
+    const char *key;
 
     (void)ctx;
 
-    key = ohm_plugin_get_param(plugin, "immts-license-key");
-    if (key == NULL) {
-        OHM_ERROR("vibra: no license key configured");
-        key = "?";
-    }
 
-    status = ImmVibeInitialize(VIBE_CURRENT_VERSION_NUMBER);
-    if (VIBE_FAILED(status)) {
-        OHM_ERROR("vibra: failed to initialize TouchSense library");
-        disabled = TRUE;
+    key = ohm_plugin_get_param(plugin, "immts-license-key");
+    dev = vibra_open();
+
+    if (dev == VIBE_INVALID_DEVICE_HANDLE_VALUE) {
+        OHM_ERROR("vibra: failed to open vibra device");
         return;
     }
     
-    status = ImmVibeOpenDevice(0, &dev);
-    if (VIBE_FAILED(status)) {
-        OHM_ERROR("vibra: failed to open TouchSense device");
-        disabled = TRUE;
+    if (!vibra_auth(dev, key)) {
+        OHM_ERROR("vibra: failed to authenticate to vibra server");
+        return;
     }
 
-    status = ImmVibeSetDevicePropertyString(dev,
-                                            VIBE_DEVPROPTYPE_LICENSE_KEY, key);
-    if (VIBE_FAILED(status)) {
-        OHM_ERROR("vibra: failed to set TouchSense license key");
-        disabled = TRUE;
+    if (!vibra_set_priority(dev, VIBE_MAX_OEM_DEVICE_PRIORITY)) {
+        OHM_ERROR("vibra: failed to set OEM vibra client priority");
+        return;
     }
 }
 
@@ -89,11 +96,7 @@ immts_exit(vibra_context_t *ctx)
 {
     (void)ctx;
 
-    if (!disabled) {
-        ImmVibeCloseDevice(dev);
-        ImmVibeTerminate();
-    }
-
+    vibra_close(dev);
     dev = VIBE_INVALID_DEVICE_HANDLE_VALUE;
 }
 
@@ -105,39 +108,159 @@ int
 immts_enforce(vibra_context_t *ctx)
 {
     vibra_group_t *group;
-    vibra_type_t   type;
-    mask_t         mask;
-    int            status;
+    uint32_t       mask;
+    int            status, muted;
 
-    mask = 0;
+    mask = IMMTS_ALL;
     for (group = ctx->groups; group->name != NULL; group++) {
-        type = group->type;
         if (group->enabled) {
             OHM_INFO("vibra: vibra group %s is now enabled", group->name);
-            mask |= immts_groups[type].mask;
+            mask |= immts_groups[group->type].mask;
         }
         else {
             OHM_INFO("vibra: vibra group %s is now disabled", group->name);
-            mask &= ~immts_groups[type].mask;
+            mask &= ~immts_groups[group->type].mask;
         }
     }
     
-    OHM_INFO("vibra: new ToucSense priority mask: 0x%x", mask);
+    OHM_INFO("vibra: new priority enable mask: 0x%x", mask);
     
-#ifdef VIB_DEVPROPTYPE_PRIORITY_MASK
-    status = ImmVibeSetDevicePropertyInt32(dev,
-                                           VIB_DEVPROPTYPE_ENABLE_PRIORITIES,
-                                           mask);
-    
-    status = disabled || !VIBE_FAILED(status)
+#ifdef VIBE_DEVPROPTYPE_PRIORITY_MASK
+    if (!vibra_enable_priorities(dev, mask))
+        OHM_ERROR("vibra: failed to enable/disable priorities");
 #else
-    OHM_INFO("*** SetDevicePropertyInt32(dev, "
-             "VIB_DEVPROPTYPE_PRIORITY_MASK, 0x%x) ***", mask);
+    muted = (mask == IMMTS_ALL);
+
+    if (!vibra_mute(dev, muted))
+        OHM_ERROR("vibra: failed to %s vibra", muted ? "mute" : "unmute");    
+#endif
     
     status = TRUE;
-#endif
 
     return status;
+}
+
+
+/********************
+ * vibra_open
+ ********************/
+static int
+vibra_open(void)
+{
+    VibeStatus status;
+    VibeInt32  device;
+
+    status = ImmVibeInitialize(VIBE_CURRENT_VERSION_NUMBER);
+    if (VIBE_FAILED(status)) {
+        OHM_ERROR("vibra: failed to initialize vibra library");
+        return VIBE_INVALID_DEVICE_HANDLE_VALUE;
+    }
+    
+    status = ImmVibeOpenDevice(0, &device);
+    if (VIBE_FAILED(status)) {
+        OHM_ERROR("vibra: failed to open vibra device");
+        return VIBE_INVALID_DEVICE_HANDLE_VALUE;
+    }
+
+    return device;
+}
+
+
+/********************
+ * vibra_close
+ ********************/
+static void
+vibra_close(VibeInt32 device)
+{
+    if (device != VIBE_INVALID_DEVICE_HANDLE_VALUE)
+        ImmVibeCloseDevice(device);
+    
+    ImmVibeTerminate();
+}
+
+
+/********************
+ * vibra_auth
+ ********************/
+static int
+vibra_auth(VibeInt32 device, const char *key)
+{
+    VibeInt32  property;
+    VibeStatus status;
+
+    if (key == NULL)
+        return FALSE;
+    
+    property = VIBE_DEVPROPTYPE_LICENSE_KEY;
+    status   = ImmVibeSetDevicePropertyString(device, property, key);
+
+    if (VIBE_FAILED(status))
+        return FALSE;
+    else
+        return TRUE;
+}
+
+
+/********************
+ * vibra_set_priority
+ ********************/
+static int
+vibra_set_priority(VibeInt32 device, int priority)
+{
+    VibeInt32  property;
+    VibeStatus status;
+    
+    property = VIBE_DEVPROPTYPE_PRIORITY;
+    status   = ImmVibeSetDevicePropertyInt32(device, property, priority);
+
+    if (VIBE_FAILED(status))
+        return FALSE;
+    else
+        return TRUE;
+}
+
+
+#ifdef VIBE_DEVPROPTYPE_PRIORITY_MASK
+/********************
+ * vibra_enable_priorities
+ ********************/
+static int
+vibra_enable_priorities(VibeInt32 device, uint32_t mask)
+{
+    VibeInt32  property;
+    VibeStatus status;
+
+
+    OHM_INFO("vibra: new ToucSense priority mask: 0x%x", mask);
+
+    property = VIBE_DEVPROPTYPE_ENABLE_PRIORITIES;
+    status   = ImmVibeSetDevicePropertyInt32(device, property, mask);
+
+    if (VIBE_FAILED(status))
+        return FALSE;
+    else
+        return TRUE;
+}
+#endif
+
+
+/********************
+ * vibra_mute
+ ********************/
+static int
+vibra_mute(VibeInt32 device, int muted)
+{
+    VibeInt32  property, volume;
+    VibeStatus status;
+    
+    property = VIBE_DEVPROPTYPE_MASTERSTRENGTH;
+    volume   = muted ? IMMTS_VOLUME_OFF : IMMTS_VOLUME_MAX;
+    status   = ImmVibeSetDevicePropertyInt32(device, property, volume);
+
+    if (VIBE_FAILED(status))
+        return FALSE;
+    else
+        return TRUE;
 }
 
 
